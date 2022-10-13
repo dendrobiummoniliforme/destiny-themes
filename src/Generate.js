ac4ecdd2b8aa420cf09196c41b1bf8dad25df2b2
d19m <84551272+dendrobiummoniliforme@users.noreply.github.com>
2022-10-13 00:15:51 -0400

    Update Generate.js (#12)

--- src/Generate.js
+++ src/Generate.js
@@ -1,14 +1,15 @@
-import { randomizeTheme, getSwatches, removeSwatchHex } from "./Theme.js";
-import { getRandomInt } from "./Math.js";
-import { log } from "./Debug.js";
-
-/**
+**
  * @author dendrobiummoniliforme.
  * @since 2022, 05, 13.
  * @description This file is used to generate a theme based off of a provided swatches.theme file.
  * @usage `npm run --silent theme > "./themes/<NAME_OF_YOUR_NEW_THEME>.json"`
  * @see https://github.com/dendrobiummoniliforme/whisper-of-the-worm/blob/main/CONTRIBUTING.md.
  */
+
+import { randomizeTheme, getSwatches, removeSwatchHex } from "./Theme.js";
+import { getRandomInt } from "./Math.js";
+import { log } from "./Debug.js";
+
 const swatches = getSwatches(process.cwd() + "/src/swatches.theme");
 const maxIntegerValue = swatches.length - 1; // Index from 0.
 const editorBackground = swatches[getRandomInt(maxIntegerValue)];
